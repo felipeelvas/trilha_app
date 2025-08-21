f3c00bd5c4ff650c596534c880d59ef320f76ba0
Felipe Elvas Barbosa <elvasfelipe@hotmail.com>
2025-08-21 11:30:13 -0300

    utilizando o widget Preview

---
+++ lib/designer/preview.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container());
+}
+
+@Preview(name: 'My Text Preview')
+Widget myText() {
+  return const Text('Hello, World!');
+}
+@Preview(name: 'My Text Field Arthur')
+Widget myTextField() {
+  return const TextField();
+}
